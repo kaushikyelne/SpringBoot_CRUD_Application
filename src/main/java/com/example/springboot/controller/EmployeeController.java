ad44dd40abd765019cf6be5a7368cdc5a31ca9ab
kaushikyelne <kaushik.s.yelne@gmail.com>
2021-04-01 01:04:48 +0530

    Creating Get Employee By Id REST API

--- src/main/java/com/example/springboot/controller/EmployeeController.java
+++ src/main/java/com/example/springboot/controller/EmployeeController.java
@@ -1,14 +1,20 @@
 package com.example.springboot.controller;
 
 import java.util.List;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springboot.exception.ResourceNotFoundException;
 import com.example.springboot.model.Employee;
 import com.example.springboot.repository.EmployeeRepository;
 
@@ -31,5 +37,10 @@ public class EmployeeController {
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
 	}
-
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee =employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee does not exist with id: " + id));
+		return ResponseEntity.ok(employee);
+	}
 }
